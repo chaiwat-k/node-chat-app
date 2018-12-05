eaf9398e749b93bed49b0dfd007d2e781c58f7c3
chaiwat-k <chaiwat.kanchanarat@gmail.com>
2018-12-05 07:56:22 +0700

    various updates

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -7,6 +9,11 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Need to go this way to incorporate socket.io
+var server = http.createServer(app);
+var io = socketIO(server);
+
+// server.lister instead of app.listen
+server.listen(port, () => {
     console.log('Server is up on port: '+port);
 });
